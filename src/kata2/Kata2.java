10f4302f6ed2b7f7767a0861dae5ca5bc64f3e1b
usuario <usuario@10.22.148.64>
2017-10-06 11:37:00 +0100

    Kata2 Versión 2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class Kata2 {
 
     public static void main(String[] args) {
-        int [] data = {1,1,1,0,0,1,2,2,2,3};
+        int [] data = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
         Map<Integer,Integer> histogram = new HashMap<>();
         
         for (int key : data) {
